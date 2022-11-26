a40cd3732ec6c48653d3e3f51662b666381fb9cf
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-26 16:15:21 +0300

    Student Instance method

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
+#Manages the model aspect of an Application 
+#This model enables us to interact with a table via Active  Record
+#Instance method which return the student's first name
+#and last name concatenated with a space between.
 class Student < ApplicationRecord
+    "#{self.first_name}: $#{self.last_name}"
 end
