5d61cdc777433aefdee36bfb9f8630a5c5ddfbac
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-26 16:45:33 +0300

    defined method to_s

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,7 @@
 #Instance method which return the student's first name
 #and last name concatenated with a space between.
 class Student < ApplicationRecord
+    def to_s
     "#{self.first_name}: $#{self.last_name}"
+    end
 end
